b37197b7304b5cd96599dbcf6e8f2f9445e15907
yuchunlin <yuchunlin@cht.com.tw>
2024-04-22 13:56:20 +0800

    init

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,15 +17,11 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
-
+    // const data = await sql<Revenue>`SELECT * FROM revenue`;
     // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
+    // return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
